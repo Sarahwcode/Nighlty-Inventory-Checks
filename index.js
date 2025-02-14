de3bf404d6e46c880dedf49cba2d012b97ca4624
Sarahwcode <sarahw181090@gmail.com>
2025-02-14 11:37:09 +0000

    test

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let string = "Hello, World!";
 let reversed = reversedString(str);
 console.log(reversed);
 function reversedString(str) {
-  return str.split("").reverse();
+  return str.split("").reverse().join("");
 }
