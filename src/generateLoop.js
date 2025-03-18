c9f364fe6403ec9c9b5f326578b1bb2a0168202e
Sarahwcode <sarahw181090@gmail.com>
2025-03-18 19:24:29 +0000

    creating a loop for entering name

--- src/generateLoop.js
+++ src/generateLoop.js
@@ -1,23 +1,37 @@
 import React, {useState} from "react";
 
 export default function GenerateLoop (){
-    let [great, useGreat] = useState("hello");
-
+    let [great, setGreat] = useState("hello");
+    let [ShowResults, setShowResults] = useState(false);
     function loop() {
-
+let results = [];
         for(let i = 0; i < 5; i++){
-            return useGreat ;
+            results.push(great);
         }
+        return results;
+    }
+
+    function enterWord(){
+setShowResults(true);
     }
     return (
         <div>
-        if (great) {
-            return (
             <div>
-            <p>{loop}</p>
-            </div> )
-        }
-       
+                <form>
+                    <label>Enter Your Name?</label>
+                    <input type="text" />
+                </form>
+            </div>
+            <button onClick={enterWord}>Show Results</button>
+      {ShowResults && (
+            
+            <div>
+            {loop().map((item, index) => (
+                <p key={index} >{item}</p>
+            ))}
+            </div> 
+            )}
+    
         </div>
-)
+);
 }
