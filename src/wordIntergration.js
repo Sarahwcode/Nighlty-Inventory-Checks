1fadac2c15f5e6b81986991b03c6fc41f8c78a3a
Sarahwcode <sarahw181090@gmail.com>
2025-03-22 10:32:16 +0000

    displaying the word definition

--- src/wordIntergration.js
+++ src/wordIntergration.js
@@ -5,17 +5,23 @@ export default function WordIntergration(){
 let [words, setWords] = useState("");
 
     function displayWords(response) {
-console.log(response.data);
+setWords(response.data);
     }
 
     function lookForWord(){
     let apiKey = "145ba0dc50b14bt75oafffd6a19b4a36";
-    let api = `https://api.shecodes.io/dictionary/v1/define?word=book&key=${apiKey}`;
+    let api = `https://api.shecodes.io/dictionary/v1/define?${words}=book&key=${apiKey}`;
     axios.get(api).then(displayWords);
 }
 
     return (
         <div>
+            <form>
+            <label>Enter a word to gain to gain more understanding</label>
+                <input type="text" onChange={displayWords}></input>
+               
+                
+            </form>
         <p><button onClick={lookForWord}>Look for word</button></p>
         </div>
     )
