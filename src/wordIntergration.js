a48841017d37a8cd1e6246c6237bf34b1cf7fcba
Sarahwcode <sarahw181090@gmail.com>
2025-03-22 10:24:34 +0000

    added api for word definition

--- src/wordIntergration.js
+++ src/wordIntergration.js
@@ -1,10 +1,22 @@
-import React from "react"
+import React, {useState} from "react";
+import axios from "axios";
 
 export default function WordIntergration(){
+let [words, setWords] = useState("");
+
+    function displayWords(response) {
+console.log(response.data);
+    }
+
+    function lookForWord(){
+    let apiKey = "145ba0dc50b14bt75oafffd6a19b4a36";
+    let api = `https://api.shecodes.io/dictionary/v1/define?word=book&key=${apiKey}`;
+    axios.get(api).then(displayWords);
+}
 
     return (
         <div>
-        <p>hello</p>
+        <p><button onClick={lookForWord}>Look for word</button></p>
         </div>
     )
 
