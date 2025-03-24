8870444c98e62fcddf78ade3ebf782efaa2950c4
Sarahwcode <sarahw181090@gmail.com>
2025-03-24 19:12:32 +0000

    added word definition

--- src/wordIntergration.js
+++ src/wordIntergration.js
@@ -2,15 +2,20 @@ import React, {useState} from "react";
 import axios from "axios";
 
 export default function WordIntergration(){
-let [words, setWords] = useState("");
+let [words, setWords] = useState(null);
+let [display, setDisplay] = useState("");
 
     function displayWords(response) {
 setWords(response.data);
+console.log(response.data);
     }
+    function displayChange (event) {
+        setDisplay(event.target.value)
+    } 
 
     function lookForWord(){
     let apiKey = "145ba0dc50b14bt75oafffd6a19b4a36";
-    let api = `https://api.shecodes.io/dictionary/v1/define?${words}=book&key=${apiKey}`;
+    let api = `https://api.shecodes.io/dictionary/v1/define?word=${display}&key=${apiKey}`;
     axios.get(api).then(displayWords);
 }
 
@@ -18,11 +23,18 @@ setWords(response.data);
         <div>
             <form>
             <label>Enter a word to gain to gain more understanding</label>
-                <input type="text" onChange={displayWords}></input>
+                <input type="text" onChange={displayChange}></input>
                
                 
             </form>
-        <p><button onClick={lookForWord}>Look for word</button></p>
+        <p><button onClick={lookForWord}>Look up Word</button></p>
+        <div>
+        {words && words.meanings && words.meanings.map((definition, index) => (
+          <p key={index}>
+            <strong>{definition.partOfSpeech}:</strong> {definition.definition}
+          </p>
+        ))}
+      </div>
         </div>
     )
 
