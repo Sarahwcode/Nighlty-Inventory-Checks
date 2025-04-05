b4e235e25f785358b233abbb211579339952a189
Sarahwcode <sarahw181090@gmail.com>
2025-04-05 13:17:12 +0100

    added more to the list

--- src/todolist.js
+++ src/todolist.js
@@ -1,8 +1,28 @@
 import React from "react";
 
 export default function ToDoList () {
+
+    function listItems (){
+let listedItems = ["Buy Food", "Exercise", "Read my book", "Study"];
+return listedItems;
+    }
     return (
     <div>
-        hello
+        {listItems().map((items, index) => (
+            <p key={index}>
+                {items}
+            </p>
+        )  )}
     </div>);
 }
+/*Create a list component: Create a new component called "List" that will display the todo list items. You can use an array of objects to store the list items and use the map() function to render each item in the list.
+
+Add form to add new items: Create a form that allows users to add new todo list items. You can use the onChange event to track changes to the form input and the onSubmit event to handle the form submission.
+
+Add a delete button: Add a delete button to each list item that allows users to delete items from the list. You can use the onClick event to handle the delete button click and update the list accordingly.
+
+Add a checkbox: Add a checkbox to each list item that allows users to mark items as complete. You can use the onChange event to handle the checkbox change and update the list accordingly.
+
+Style the app: Use CSS or a CSS framework like Bootstrap to style the app and make it look visually appealing.
+
+By following these steps, you should be able to build a simple todo list app using ReactJS. You can then expand on this app by adding more features or customizing it to fit your specific needs.*/
