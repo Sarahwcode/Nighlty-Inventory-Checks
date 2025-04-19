490ee2a40bfe3d649b81f56d3dd4b100b675882e
Sarahwcode <sarahw181090@gmail.com>
2025-04-19 14:53:48 +0100

    adding operators and numbers

--- src/Calculator.js
+++ src/Calculator.js
@@ -7,11 +7,22 @@ export default function Calculator (){
     function calculator (operation) {
  if (operation === "+")
     setSums();
+ else if (operation === "-")
+    setSums();
+  else if (operation === "*")
+    setSums();
     }
 
     return (
         <div>
-            <button onClick={() => calculator("+")}>+</button>
+            <div><button onClick={() => calculator("+")}>+</button>
+            <button onClick={() => calculator("-")}>-</button>
+        <button onClick={() => calculator("*")}>x</button></div>
+            <button>1</button> <button>2</button> <button>3</button> 
+            <div><button>4</button> <button>5</button><button>6</button></div>
+            <div><button>6</button> <button>7</button><button>9</button></div>
+            <div><button>0</button></div>
+
 <input value={sums}/>
         </div>
     )
